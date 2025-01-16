fb3875bf569715ef0753882fda1f67c706d01307
bryan-genailabs <alfredo@generatives.co>
2025-01-16 23:22:30 +0800

    change placeholder image

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
           <div className="flex justify-center items-center">
             <div className="relative w-72 h-72 sm:w-96 sm:h-96">
               <Image
-                src="https://raw.githubusercontent.com/ALGOREX-PH/AI-Republic-Student-Portfolio-Template/refs/heads/main/app/images/Erwin.jpeg" // Change to your Photo Please
+                src="https://placehold.co/600x600/purple/white?text=Your+Profile+Photo"
                 alt="Profile"
                 fill
                 className="rounded-3xl object-cover"
